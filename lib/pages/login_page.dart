44ea9ad7d2f5628f8d1a810cadd9bfd56b8154d4
HaDeS-18 <harsh.agrawal116611@gmail.com>
2024-08-06 20:52:34 +0530

    Initial Commit - What's new    Authentication Pages and Backend

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,100 @@
+
+import 'package:flutter/material.dart';
+import 'package:minimal_chat_app/components/my_button.dart';
+import 'package:minimal_chat_app/components/my_textfield.dart';
+
+class LoginPage extends StatelessWidget {
+
+  //email and pw controller
+  final TextEditingController _emailController  = TextEditingController();
+  final TextEditingController _pwController  = TextEditingController();
+
+  //login method
+  void login(){
+   //Define login method 
+  }
+
+  //tap to go to register now page
+  final void Function() onTap;
+
+  LoginPage({super.key,required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //logo
+            Icon(
+              Icons.message,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+              ),
+
+            const SizedBox(
+                height: 50,
+              ),
+            //welcome back message
+            Text(
+              'Welcome back, you have been missed!',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+            ),
+
+            const SizedBox(height: 20,),
+            //email textfield
+
+            MyTextfield(
+              controller: _emailController,
+              obscureText: false,
+              hintText: 'Email'
+              ),
+
+            const SizedBox(height: 10),
+
+            //pw textfield
+            MyTextfield(
+              controller: _pwController,
+              obscureText: true,
+              hintText: 'password'
+              ),
+
+            const SizedBox(height: 25),
+            //login button
+
+            MyButton(
+              text: "Login",
+              onTap: login,
+              ),
+
+            const SizedBox(height: 25,),
+            //register now
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Not a member?",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                ),),
+                GestureDetector(
+                  onTap: onTap,
+                  child: Text("Register Now",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                                ),
+                ),
+              ],
+            ),
+          ],
+          ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/register_page.dart
@@ -0,0 +1,108 @@
+
+import 'package:flutter/material.dart';
+import 'package:minimal_chat_app/components/my_button.dart';
+import 'package:minimal_chat_app/components/my_textfield.dart';
+
+class RegisterPage extends StatelessWidget {
+  //email, pw and conf pw controllers
+  final TextEditingController _confirmPwController = TextEditingController();
+  final TextEditingController _emailController  = TextEditingController();
+  final TextEditingController _pwController  = TextEditingController();
+
+  //login method
+  void register(){
+   //Define login method 
+  }
+  //tap to go to register now page
+  final void Function() onTap;
+
+  RegisterPage({super.key,required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //logo
+            Icon(
+              Icons.message,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+              ),
+
+            const SizedBox(
+                height: 50,
+              ),
+            //welcome back message
+            Text(
+              "Let's create an account for you",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+            ),
+
+            const SizedBox(height: 20,),
+            //email textfield
+
+            MyTextfield(
+              controller: _emailController,
+              obscureText: false,
+              hintText: 'Email'
+              ),
+
+            const SizedBox(height: 10),
+
+            //pw textfield
+            MyTextfield(
+              controller: _pwController,
+              obscureText: true,
+              hintText: 'Password'
+              ),
+            
+            const SizedBox(height: 10),
+
+            //conf pw textfield
+            MyTextfield(
+              controller: _confirmPwController,
+              obscureText: true,
+              hintText: 'Confirm password'
+              ),
+
+            const SizedBox(height: 25),
+            //register button
+
+            MyButton(
+              text: "Register",
+              onTap: register,
+              ),
+
+            const SizedBox(height: 25,),
+            //register now
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Already a member?",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                ),),
+                GestureDetector(
+                  onTap: onTap,
+                  child: Text("Login now",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                                ),
+                ),
+              ],
+            ),
+          ],
+          ),
+      ),
+    );
+  }
+}
